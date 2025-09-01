186b4979e5e5db32fd754162d35c3cca5b112bac
lxymahatma <lxymahatma@gmail.com>
2025-09-01 15:52:32 +0800

    fix windows service get game path

--- src/MuseDashModTools.Core/Services/PlatformServices/WindowsService/WindowsService.Private.cs
+++ src/MuseDashModTools.Core/Services/PlatformServices/WindowsService/WindowsService.Private.cs
@@ -6,13 +6,34 @@ namespace MuseDashModTools.Core;
 
 internal sealed partial class WindowsService
 {
-    private IEnumerable<string> GetSteamLibraries()
-    {
-        var stream = File.OpenRead(Path.Combine(Config.SteamFolder, @"steamapps\libraryfolders.vdf"));
-        var kv = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
-        var data = kv.Deserialize<Dictionary<string, LibraryFolder>>(stream);
+    private IEnumerable<string> GetCandidateFolders() =>
+        TryGetSteamLibraries(out var libraryFolders) ? libraryFolders : WindowsPaths;
 
-        return data.Values.Select(x => x.Path.Replace(@"\\", @"\"));
+    private bool TryGetSteamLibraries(out IEnumerable<string> libraryFolders)
+    {
+        libraryFolders = [];
+
+        var vdfPath = Path.Combine(Config.SteamFolder, @"steamapps\libraryfolders.vdf");
+        if (!File.Exists(vdfPath))
+        {
+            Logger.ZLogWarning($"Steam libraryfolders.vdf not found at {vdfPath}");
+            return false;
+        }
+
+        try
+        {
+            var stream = File.OpenRead(vdfPath);
+            var kv = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
+            var data = kv.Deserialize<Dictionary<string, LibraryFolder>>(stream);
+
+            libraryFolders = data.Values.Select(x => x.Path.Replace(@"\\", @"\"));
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Logger.ZLogError(ex, $"Deserialize libraryfolders.vdf failed");
+            return false;
+        }
     }
 
 
