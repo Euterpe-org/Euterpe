9086035a21f4b6924853f7702166a64f748591dd
lxymahatma <lxymahatma@gmail.com>
2025-09-02 15:32:09 +0800

    add show start screen option

--- src/MuseDashModTools.Core/Services/PlatformServices/MacOSService.cs
+++ src/MuseDashModTools.Core/Services/PlatformServices/MacOSService.cs
@@ -15,7 +15,8 @@ internal sealed class MacOsService : IPlatformService
     public bool TryGetGameFolder([NotNullWhen(true)] out string? gameFolder) => throw new NotSupportedException();
     public bool CheckIsValidSteamFolder(string folderPath) => throw new NotSupportedException();
     public bool CheckIsValidGameFolder(string folderPath) => throw new NotSupportedException();
-    public Task<bool> LaunchGameWithArgsAsync(string gameId, string launchArguments) => throw new NotSupportedException();
+    public Task<bool> LaunchGameWithArgAsync(string gameId, string launchArgument) => throw new NotSupportedException();
+    public Task<bool> LaunchGameWithArgsAsync(string gameId, IEnumerable<string> launchArguments) => throw new NotSupportedException();
     public Task<bool> InstallDotNetRuntimeAsync() => throw new NotSupportedException();
     public Task<bool> InstallDotNetSdkAsync() => throw new NotSupportedException();
     public void RevealFile(string filePath) => throw new NotSupportedException();
