62a04e4a3072cc958f7c7f91061b39aa84eed44d
lxymahatma <lxymahatma@gmail.com>
2025-10-14 14:38:40 +0900

    async archive

--- src/MuseDashModTools.Abstractions/IArchiveService.cs
+++ src/MuseDashModTools.Abstractions/IArchiveService.cs
@@ -3,5 +3,7 @@
 public interface IArchiveService
 {
     bool CreateZipFile(string sourceFolder, string zipPath);
+    Task<bool> CreateZipFileAsync(string sourceFolder, string zipPath);
     bool ExtractZipFile(string zipPath, string extractPath);
+    Task<bool> ExtractZipFileAsync(string zipPath, string extractPath);
 }
