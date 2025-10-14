2d357e36ec1b324ca8e2fba642ae090be00b9cd0
lxymahatma <lxymahatma@gmail.com>
2025-10-14 14:38:40 +0900

    async archive

--- src/MuseDashModTools.Core/Services/ArchiveService.cs
+++ src/MuseDashModTools.Core/Services/ArchiveService.cs
@@ -1,4 +1,4 @@
-﻿using System.IO.Compression;
+using System.IO.Compression;
 
 namespace MuseDashModTools.Core;
 
@@ -31,11 +31,46 @@ internal sealed class ArchiveService : IArchiveService
         }
     }
 
+    public async Task<bool> CreateZipFileAsync(string sourceFolder, string zipPath)
+    {
+        try
+        {
+            if (File.Exists(zipPath))
+            {
+                File.Delete(zipPath);
+            }
+
+            await ZipFile.CreateFromDirectoryAsync(sourceFolder, zipPath, CompressionLevel.Optimal, false).ConfigureAwait(false);
+            Logger.ZLogInformation($"Successfully created {zipPath} from {sourceFolder}");
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Logger.ZLogError(ex, $"Failed to create zip file {zipPath} from {sourceFolder}");
+            return false;
+        }
+    }
+
     public bool ExtractZipFile(string zipPath, string extractPath)
     {
         try
         {
             ZipFile.ExtractToDirectory(zipPath, extractPath, true);
+            Logger.ZLogInformation($"Successfully extracted {zipPath} to {extractPath}");
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Logger.ZLogError(ex, $"Failed to extract file {zipPath} to {extractPath}");
+            return false;
+        }
+    }
+
+    public async Task<bool> ExtractZipFileAsync(string zipPath, string extractPath)
+    {
+        try
+        {
+            await ZipFile.ExtractToDirectoryAsync(zipPath,extractPath,true).ConfigureAwait(false);
             Logger.ZLogInformation($"Successfully extracted {zipPath} to {extractPath}");
             return true;
         }
