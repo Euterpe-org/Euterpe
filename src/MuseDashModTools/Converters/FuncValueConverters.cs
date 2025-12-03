366a9e5199f2b8cf48520445468f860e0cc66918
lxymahatma <lxymahatma@gmail.com>
2025-12-03 16:26:31 +0900

    remove related code

--- src/MuseDashModTools/Converters/FuncValueConverters.cs
+++ src/MuseDashModTools/Converters/FuncValueConverters.cs
@@ -1,4 +1,4 @@
-﻿using Avalonia.Media;
+using Avalonia.Media;
 
 namespace MuseDashModTools.Converters;
 
@@ -9,19 +9,4 @@ public static class FuncValueConverters
 
     public static FuncValueConverter<string, StreamGeometry?> SemiIconConverter { get; } =
         new(iconKeyName => _resourceService.TryGetAppResource<StreamGeometry>($"{IconPrefix}{iconKeyName}"));
-
-    public static FuncValueConverter<string?, string?> ChartSceneConverter { get; } =
-        new(scene => scene switch
-        {
-            "scene_01" => XAML_Scene_SpaceStation,
-            "scene_02" => XAML_Scene_Retrocity,
-            "scene_03" => XAML_Scene_Castle,
-            "scene_04" => XAML_Scene_RainyNight,
-            "scene_05" => XAML_Scene_Candyland,
-            "scene_06" => XAML_Scene_Oriental,
-            "scene_07" => XAML_Scene_LetsGroove,
-            "scene_08" => XAML_Scene_Touhou,
-            "scene_09" => XAML_Scene_DJMAX,
-            _ => scene
-        });
 }
