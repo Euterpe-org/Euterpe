1de20485af89277a63acf9d5152f1996b5643000
KARPED1EM <leever.zzz@gmail.com>
2026-01-26 11:30:18 +0800

    refactor: donate button refers to website instead of DonationDialog component

--- src/Euterpe/ViewModels/Pages/HomePageViewModel.cs
+++ src/Euterpe/ViewModels/Pages/HomePageViewModel.cs
@@ -1,6 +1,3 @@
-using Ursa.Common;
-using Ursa.Controls.Options;
-
 namespace Euterpe.ViewModels.Pages;
 
 public sealed partial class HomePageViewModel : ViewModelBase
@@ -21,24 +18,6 @@ public sealed partial class HomePageViewModel : ViewModelBase
     }
 
     [RelayCommand]
-    private Task<DialogResult> ShowDonationDrawerAsync()
-    {
-        var options = new DrawerOptions
-        {
-            Position = Position.Right,
-            Title = "原神启动",
-            IsCloseButtonVisible = true,
-            CanLightDismiss = true,
-            MaxWidth = 700,
-            MinWidth = 700,
-            Buttons = DialogButton.None,
-            CanResize = false
-        };
-
-        return Drawer.ShowModal<DonationDialog, DonationDialogViewModel>(DonationDialogViewModel, "DonationDrawerHost", options);
-    }
-
-    [RelayCommand]
     private Task LaunchGameAsync()
     {
         return Config.GameMode switch
@@ -56,9 +35,6 @@ public sealed partial class HomePageViewModel : ViewModelBase
 
     [UsedImplicitly]
     public required Config Config { get; init; }
-
-    [UsedImplicitly]
-    public required DonationDialogViewModel DonationDialogViewModel { get; init; }
 
     [UsedImplicitly]
     public required IGameService GameService { get; init; }
