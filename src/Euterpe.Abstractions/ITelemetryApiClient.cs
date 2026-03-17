265d83e25fd1e90a44d668e6ade2b3af069ae436
lxymahatma <lxymahatma@gmail.com>
2026-03-17 21:41:51 +0800

    fixing post body

--- src/Euterpe.Abstractions/ITelemetryApiClient.cs
+++ src/Euterpe.Abstractions/ITelemetryApiClient.cs
@@ -7,8 +7,8 @@ namespace Euterpe.Abstractions;
 public interface ITelemetryApiClient
 {
     [Post(Telemetry.Session)]
-    Task<HttpResponseMessage> PostSessionAsync(SessionEvent payload, CancellationToken cancellationToken = default);
+    Task<HttpResponseMessage> PostSessionAsync([Body] SessionEvent payload, CancellationToken cancellationToken = default);
 
     [Post(Telemetry.ModDownload)]
-    Task<HttpResponseMessage> PostModDownloadAsync(ModDownloadEvent payload, CancellationToken cancellationToken = default);
+    Task<HttpResponseMessage> PostModDownloadAsync([Body] ModDownloadEvent payload, CancellationToken cancellationToken = default);
 }
