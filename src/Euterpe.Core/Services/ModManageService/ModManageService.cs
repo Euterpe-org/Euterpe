846cdc23fa8c9b4c787d6208f8ff442a9d406f41
lxymahatma <lxymahatma@gmail.com>
2026-03-17 21:29:12 +0800

    fix thread issue

--- src/Euterpe.Core/Services/ModManageService/ModManageService.cs
+++ src/Euterpe.Core/Services/ModManageService/ModManageService.cs
@@ -25,7 +25,7 @@ internal sealed partial class ModManageService : IModManageService
     public async Task InstallModAsync(ModDto mod)
     {
         Logger.ZLogInformation($"Installing mod: {mod.Name}");
-        await DownloadModCoreAsync(mod).ConfigureAwait(false);
+        await DownloadModCoreAsync(mod).ConfigureAwait(true);
         Logger.ZLogInformation($"Mod {mod.Name} successfully installed");
         NotificationService.SuccessLight(Notification_Content_Mod_Install_Success, mod.Name);
     }
@@ -34,7 +34,7 @@ internal sealed partial class ModManageService : IModManageService
     {
         Logger.ZLogInformation($"Updating mod: {mod.Name} from version {mod.LocalVersion} to version {mod.Version}");
         File.Delete(Path.Combine(Config.ModsFolder, mod.LocalFileName));
-        await DownloadModCoreAsync(mod).ConfigureAwait(false);
+        await DownloadModCoreAsync(mod).ConfigureAwait(true);
         Logger.ZLogInformation($"Mod {mod.Name} successfully updated to version {mod.Version}");
         NotificationService.SuccessLight(Notification_Content_Mod_Update_Success, mod.Name);
     }
@@ -43,7 +43,7 @@ internal sealed partial class ModManageService : IModManageService
     {
         Logger.ZLogInformation($"Reinstalling mod: {mod.Name}");
         File.Delete(Path.Combine(Config.ModsFolder, mod.LocalFileName));
-        await DownloadModCoreAsync(mod).ConfigureAwait(false);
+        await DownloadModCoreAsync(mod).ConfigureAwait(true);
         Logger.ZLogInformation($"Mod {mod.Name} successfully reinstalled");
         NotificationService.SuccessLight(Notification_Content_Mod_Reinstall_Success, mod.Name);
     }
@@ -52,7 +52,7 @@ internal sealed partial class ModManageService : IModManageService
     {
         Logger.ZLogInformation($"Uninstalling mod: {mod.Name}");
         File.Delete(Path.Combine(Config.ModsFolder, mod.LocalFileName));
-        await DisableModDependentsAsync(mod).ConfigureAwait(false);
+        await DisableModDependentsAsync(mod).ConfigureAwait(true);
         mod.RemoveLocalInfo();
         Logger.ZLogInformation($"Mod {mod.Name} successfully uninstalled");
         NotificationService.SuccessLight(Notification_Content_Mod_Uninstall_Success, mod.Name);
