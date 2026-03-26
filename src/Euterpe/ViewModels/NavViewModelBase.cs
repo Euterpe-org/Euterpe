fbaed3732c972843dd363bf301aead19d58f1b06
lxymahatma <lxymahatma@gmail.com>
2026-03-27 00:01:18 +0900

    remove unnecessary check

--- src/Euterpe/ViewModels/NavViewModelBase.cs
+++ src/Euterpe/ViewModels/NavViewModelBase.cs
@@ -3,22 +3,16 @@ namespace Euterpe.ViewModels;
 public abstract partial class NavViewModelBase : ViewModelBase
 {
     [ObservableProperty]
-    public partial Control? Content { get; set; }
+    public partial Control Content { get; set; } = null!;
 
     [ObservableProperty]
-    public partial NavItem? SelectedItem { get; set; }
+    public partial NavItem SelectedItem { get; set; } = null!;
 
     public abstract IReadOnlyList<NavItem> NavItems { get; }
 
     protected abstract Control ResolveRoute(string route);
 
-    partial void OnSelectedItemChanged(NavItem? value)
-    {
-        if (value is not null)
-        {
-            Content = ResolveRoute(value.NavigateKey);
-        }
-    }
+    partial void OnSelectedItemChanged(NavItem value) => Content = ResolveRoute(value.NavigateKey);
 
     public override Task InitializeAsync()
     {
