7f603f0a85130af3d70bb6223fffde85f8444573
lxymahatma <lxymahatma@gmail.com>
2026-03-27 00:28:36 +0900

    xmlns euterpe rename to e

--- src/Euterpe.Styles/Properties/AssemblyInfo.cs
+++ src/Euterpe.Styles/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 [assembly: XmlnsDefinition("https://github.com/Euterpe-org/Euterpe", "Euterpe.Styles.ExtendedControls")]
 [assembly: XmlnsDefinition("https://github.com/Euterpe-org/Euterpe", "Euterpe.Styles.TemplatedControls")]
-[assembly: XmlnsPrefix("https://github.com/Euterpe-org/Euterpe", "euterpe")]
+[assembly: XmlnsPrefix("https://github.com/Euterpe-org/Euterpe", "e")]
