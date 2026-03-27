c0c93064cdc416461a58eaacfd55f822dd84db7a
lxymahatma <lxymahatma@gmail.com>
2026-03-28 03:00:57 +0900

    format code

--- src/Euterpe.Core/Proxies/TopLevelProxy.cs
+++ src/Euterpe.Core/Proxies/TopLevelProxy.cs
@@ -2,10 +2,11 @@ using Avalonia.Platform.Storage;
 
 namespace Euterpe.Core.Proxies;
 
-[UsedImplicitly]
 public sealed class TopLevelProxy
 {
+    [UsedImplicitly]
     public required Func<TopLevel> TopLevelFactory { get; init; }
+
     private TopLevel Current => TopLevelFactory();
 
     public IStorageProvider StorageProvider => Current.StorageProvider;
