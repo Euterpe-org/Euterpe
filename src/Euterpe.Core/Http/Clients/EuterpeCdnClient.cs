6ea606096abf10a21c8787cdde0656716ce9e2d4
lxymahatma <lxymahatma@gmail.com>
2026-04-12 18:14:25 +0900

    change clients to be public

--- src/Euterpe.Core/Http/Clients/EuterpeCdnClient.cs
+++ src/Euterpe.Core/Http/Clients/EuterpeCdnClient.cs
@@ -4,7 +4,7 @@ using static Euterpe.Shared.EuterpeCdn;
 
 namespace Euterpe.Core.Http.Clients;
 
-internal sealed class EuterpeCdnClient(HttpClient client)
+public sealed class EuterpeCdnClient(HttpClient client)
 {
     public async Task<Mod[]> FetchModListAsync(CancellationToken cancellationToken = default) =>
         await client.GetFromJsonAsync<Mod[]>(Assets.ModsJsonUrl, Default.ModArray, cancellationToken).ConfigureAwait(false) ?? [];
