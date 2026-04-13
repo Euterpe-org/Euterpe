0acd138d2b4848392d7adaa9b765258fe0b52c21
lxymahatma <lxymahatma@gmail.com>
2026-04-14 02:57:02 +0900

    refactor

---
+++ src/Euterpe.Core/Extensions/RefitExtensions.cs
@@ -0,0 +1,29 @@
+using Euterpe.Core.Http.Handlers;
+using Euterpe.Core.JsonContexts;
+using Refit;
+
+namespace Euterpe.Core.Extensions;
+
+public static class RefitExtensions
+{
+    private static readonly RefitSettings RefitSettings = new()
+    {
+        ContentSerializer = new SystemTextJsonContentSerializer(SnakeCaseJsonContext.Default.Options)
+    };
+
+    public static IHttpClientBuilder AddEuterpeRefitClient<T>(this IServiceCollection services, string name, string basePath, bool authenticated = false)
+        where T : class
+    {
+        var builder = services
+            .AddRefitClient<T>(RefitSettings, name)
+            .ConfigureHttpClient(c => c.BaseAddress = new Uri($"{EuterpeApi.BaseUrl}{basePath}"))
+            .AddHttpMessageHandler<XRequestIdHandler>();
+
+        if (authenticated)
+        {
+            builder.AddHttpMessageHandler<AuthHeaderHandler>();
+        }
+
+        return builder;
+    }
+}
