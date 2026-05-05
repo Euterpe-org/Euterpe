0d74e4114b8ecbe133ac4b9d3a0b23c23dad3832
lxymahatma <lxymahatma@gmail.com>
2026-05-06 04:46:40 +0900

    add GameSwitcher service

--- src/Euterpe/Extensions/ServiceExtensions.cs
+++ src/Euterpe/Extensions/ServiceExtensions.cs
@@ -9,6 +9,7 @@ public static partial class ServiceExtensions
     {
         // Self Services
         builder.RegisterType<DeepLinkService>().PropertiesAutowired().SingleInstance();
+        builder.RegisterType<GameSwitcher>().PropertiesAutowired().SingleInstance();
         builder.RegisterType<NavigationService>().PropertiesAutowired().SingleInstance();
         builder.RegisterType<LocalizationService>().PropertiesAutowired().SingleInstance();
 
