4373153e67b393b644fbbdd3d583f543745e43a7
lxymahatma <lxymahatma@gmail.com>
2026-05-06 15:20:19 +0900

    localization for status

--- src/Euterpe.Models/Games/WizardStepState.cs
+++ src/Euterpe.Models/Games/WizardStepState.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Euterpe.Models.Games;
 
 public sealed partial class WizardStepState(WizardOption option) : ObservableObject
@@ -10,10 +12,20 @@ public sealed partial class WizardStepState(WizardOption option) : ObservableObj
 
     [ObservableProperty]
     [NotifyPropertyChangedFor(nameof(CanRetry))]
+    [NotifyPropertyChangedFor(nameof(StatusDisplay))]
     public partial WizardStepStatus Status { get; set; }
 
     [ObservableProperty]
     public partial string? ErrorMessage { get; set; }
 
     public bool CanRetry => Status is WizardStepStatus.Failed;
+
+    public LocalizedString StatusDisplay => Status switch
+    {
+        WizardStepStatus.Pending => Wizard_StepStatus_Pending,
+        WizardStepStatus.Running => Wizard_StepStatus_Running,
+        WizardStepStatus.Succeeded => Wizard_StepStatus_Succeeded,
+        WizardStepStatus.Failed => Wizard_StepStatus_Failed,
+        _ => throw new UnreachableException()
+    };
 }
