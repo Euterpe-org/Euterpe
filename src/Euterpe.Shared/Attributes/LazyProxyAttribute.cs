0ad52db14f089c9489e6d35dfac1d97b17f5c282
lxymahatma <lxymahatma@gmail.com>
2026-05-11 19:58:33 +0900

    remove unused lazyproxy

--- src/Euterpe.Shared/Attributes/LazyProxyAttribute.cs
+++
@@ -1,5 +0,0 @@
-﻿#pragma warning disable CS9113 // Parameter is unread.
-namespace Euterpe.Shared.Attributes;
-
-[AttributeUsage(AttributeTargets.Class)]
-public sealed class LazyProxyAttribute(Type baseType) : Attribute;
