aeccd5b9c48d6099e7c6cdc28e2d3ff32df9c031
KARPED1EM <leever.zzz@gmail.com>
2026-05-31 11:59:50 +0800

    move LoopbackCallbackResult to Euterpe.Contracts.Account

--- src/Euterpe.Contracts/Account/LoopbackCallbackResult.cs
+++ src/Euterpe.Contracts/Account/LoopbackCallbackResult.cs
@@ -1,3 +1,4 @@
-namespace Euterpe.Abstractions;
+namespace Euterpe.Contracts.Account;
 
+[PublicAPI]
 public sealed record LoopbackCallbackResult(string? Code, string? State, string? Error);
--- src/Euterpe.Core/Services/AuthService/LoopbackCallbackListener.cs
+++ src/Euterpe.Core/Services/AuthService/LoopbackCallbackListener.cs
@@ -2,6 +2,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Web;
+using Euterpe.Contracts.Account;
 
 namespace Euterpe.Core;
 
