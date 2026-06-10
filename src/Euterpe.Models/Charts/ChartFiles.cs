ace16a38386077ed0579533949f3bb84417d6258
lxymahatma <lxymahatma@gmail.com>
2026-06-11 05:22:56 +0900

    perf: MapFileName builds its name in one interpolation

--- src/Euterpe.Models/Charts/ChartFiles.cs
+++ src/Euterpe.Models/Charts/ChartFiles.cs
@@ -26,7 +26,7 @@ public static class ChartFiles
 
     public static string MapName(ChartDifficulty difficulty) => $"map{(int)difficulty}";
 
-    public static string MapFileName(ChartDifficulty difficulty) => $"{MapName(difficulty)}.bms";
+    public static string MapFileName(ChartDifficulty difficulty) => $"map{(int)difficulty}.bms";
 
     public static IReadOnlyList<ChartDifficulty> ExistingDifficulties(this IReadOnlyDictionary<string, ManifestFileEntry> files) =>
         [.. AllDifficulties.Where(difficulty => files.ContainsKey(MapFileName(difficulty)))];
