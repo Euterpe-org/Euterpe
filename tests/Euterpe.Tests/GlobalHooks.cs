1cf586132456b14f1b8e2a01321907454770d3a6
lxymahatma <lxymahatma@gmail.com>
2026-06-15 16:02:31 +0900

    pin the test session to invariant culture so localized assertions are locale-independent

--- tests/Euterpe.Tests/GlobalHooks.cs
+++ tests/Euterpe.Tests/GlobalHooks.cs
@@ -1,3 +1,15 @@
+using System.Globalization;
+using Euterpe.Localization;
+
 namespace Euterpe.Tests;
 
-public static class GlobalHooks;
+public static class GlobalHooks
+{
+    [Before(TestSession)]
+    public static void UseInvariantCulture()
+    {
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+        CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+        LocalizationManager.Culture = CultureInfo.InvariantCulture;
+    }
+}
