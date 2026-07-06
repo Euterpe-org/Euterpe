e06a2e2ec6d9d3e2bdd3238ee8cbf22691f6d98a
lxymahatma <lxymahatma@gmail.com>
2026-07-06 19:15:41 +0900

    delete old backups in the updater

--- src/Euterpe/Program.cs
+++ src/Euterpe/Program.cs
@@ -25,7 +25,6 @@ internal static class Program
 
         Directory.CreateDirectory(AppDataFolder);
         CleanupLogFiles();
-        CleanupBackupFiles();
         ConfigureContainer();
         StartActivationPipeServer();
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
