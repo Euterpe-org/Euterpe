01c78dcc3c19a520aa6f0c7afdc3a8745e2ac1c1
lxymahatma <lxymahatma@gmail.com>
2026-07-06 19:13:20 +0900

    robustify the updater

--- src/Euterpe.Updater/Contracts/ILocalService.cs
+++ src/Euterpe.Updater/Contracts/ILocalService.cs
@@ -2,6 +2,8 @@ namespace Euterpe.Updater.Contracts;
 
 public interface ILocalService
 {
+    bool IsReadableZipFile(string zipPath);
     void ExtractZipFile(string zipPath, string extractPath);
     void CopyDirectory(string sourceDir, string destinationDir);
+    bool TryDeleteFile(string filePath);
 }
