98a18adcab44b804ebaabd333374e67e9e051abd
lxymahatma <lxymahatma@gmail.com>
2026-07-23 22:44:39 +0800

    move log files into local appdata

--- src/Euterpe.Shared/PathConstants.cs
+++ src/Euterpe.Shared/PathConstants.cs
@@ -3,6 +3,7 @@ namespace Euterpe.Shared;
 public static class PathConstants
 {
     public static readonly string AppDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), AppName);
-    public static readonly string AppLogsFolder = Path.Combine(AppContext.BaseDirectory, "Logs");
+    public static readonly string LocalAppDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), AppName);
+    public static readonly string AppLogsFolder = Path.Combine(LocalAppDataFolder, "Logs");
     public static readonly string LogFilePath = Path.Combine(AppLogsFolder, $"{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.log");
 }
