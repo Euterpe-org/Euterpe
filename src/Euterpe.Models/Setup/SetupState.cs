c7460d30541b4542ab8fd5f42042444d3e785519
lxymahatma <lxymahatma@gmail.com>
2026-07-24 15:03:18 +0800

    add reveal log action to setup execution page

--- src/Euterpe.Models/Setup/SetupState.cs
+++ src/Euterpe.Models/Setup/SetupState.cs
@@ -9,11 +9,14 @@ public sealed partial class SetupState : ObservableObject
     [ObservableProperty]
     [NotifyPropertyChangedFor(nameof(IsRunning))]
     [NotifyPropertyChangedFor(nameof(AllSucceeded))]
+    [NotifyPropertyChangedFor(nameof(HasFailedSteps))]
     public partial SetupExecutionStage Stage { get; set; }
 
     public bool IsRunning => Stage is SetupExecutionStage.Running;
 
     public bool AllSucceeded => Stage is SetupExecutionStage.Finished && Steps.All(s => s.Status is SetupStepStatus.Succeeded);
+
+    public bool HasFailedSteps => Stage is SetupExecutionStage.Finished && Steps.Any(s => s.Status is SetupStepStatus.Failed);
 
     public void Reset()
     {
