6ace945361b82211bbe3ae4bcd936ad452d8f169
lxymahatma <lxymahatma@gmail.com>
2026-08-12 19:48:39 +0800

    refactor GetLocalPaths

--- src/Euterpe.Core/Extensions/StorageItemExtensions.cs
+++ src/Euterpe.Core/Extensions/StorageItemExtensions.cs
@@ -4,6 +4,6 @@ namespace Euterpe.Core.Extensions;
 
 public static class StorageItemExtensions
 {
-    public static IEnumerable<string?> GetLocalPaths(this IReadOnlyList<IStorageItem> items) =>
-        items.Select(item => item.TryGetLocalPath());
+    public static IEnumerable<string> GetLocalPaths(this IReadOnlyList<IStorageItem> items) =>
+        items.Select(item => item.TryGetLocalPath()).OfType<string>();
 }
